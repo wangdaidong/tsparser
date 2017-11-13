8d747f3bb0f964c23f78750f9f17e4f4f30d39f3
wdd <411398239@qq.com>
2017-11-13 10:51:25 +0800

    parse transport stream
    Signed-off-by: wdd <411398239@qq.com>

---
+++ src/ts.cpp
@@ -0,0 +1,528 @@
+/*
+ * ts.cpp
+ *
+ *  Created on: 2017年8月3日
+ *      Author: root
+ */
+
+#include "ts.h"
+
+namespace ts {
+
+/**
+ * 解析ts的包头
+ * @param buff
+ */
+void ts_header::parse(const unsigned char* buff) {
+	sync_byte = buff[0];
+	transport_error_indicator = buff[1] & 0x80;
+	payload_unit_start_indicator = buff[1] & 0x40;
+	transport_priority = buff[1] & 0x20;
+	pid = ((buff[1] & 0x1f) << 8) + buff[2];
+	transport_scrambling_control = (buff[3] & 0xc0) >> 6;
+	adaption_field_control = (buff[3] & 0x30) >> 4;
+	continuity_counter = buff[3] & 0x0f;
+
+	//判断同步字节
+	if (sync_byte != 0x47) {
+		playload_offset = -1;
+		return;
+	}
+
+	//判断传输错误标志
+	if (transport_error_indicator) {
+		playload_offset = -2;
+		return;
+	}
+
+	//adaption_field_control==2 无playload data
+	if (adaption_field_control == 2) {
+		playload_offset = -3;
+		return;
+	}
+
+	//跳过header获取数据
+	playload_offset += 4;
+
+	//adaption_field_control==3 跳过adaption_field 读取playload data
+	if (adaption_field_control == 3)
+		playload_offset += buff[4] + 1;
+
+	//跳过data_byte 读取playload data ISO/IEC 13818-1 p19
+	//tsheader.playload_offset += 1;
+	//return tsheader;
+}
+
+/**
+ * 解析SDT
+ * @param buff
+ */
+void ts_sdt::parse(const unsigned char* buff) {
+	buff++;
+	table_id = buff[0]; //8
+	section_syntax_indicator = (buff[1] & 0x80) >> 7; //1 通常设为“1”
+	reserved_future_use_0 = (buff[1] & 0x40) >> 6; //1
+	reserved_0 = (buff[1] & 0x30) >> 4; //2
+	section_length = ((buff[1] & 0x0f) << 8) + buff[2]; //12
+	transport_stream_id = (buff[3] << 8) + buff[4]; //16 给出TS识别号
+	reserved_1 = (buff[5] & 0xc0) >> 6;
+	version_number = (buff[5] & 0x3e) >> 1; //5
+	current_next_indicator = buff[5] & 0x01; //1
+	section_number = buff[6]; //8
+	last_section_number = buff[7]; //8
+	original_nerwork_id = (buff[8] << 8) + buff[9]; //16
+	reserved_future_use_1 = buff[10]; //8
+
+	service_id = (buff[11] << 8) + buff[12]; //16
+	reserved_future_use_2 = (buff[13] & 0xfc) >> 2; //6
+	EIT_schedule_flag = (buff[13] & 0x02) >> 1; //1
+	EIT_present_following_flag = (buff[13] & 0x01); //1
+	running_status = (buff[14] & 0xe0) >> 5; //3
+	freed_CA_mode = (buff[14] & 0x10) >> 4; //1
+	descriptors_loop_length = ((buff[14] & 0x0f) << 8) + buff[15]; //12
+	descriptor_tag = buff[16]; //8
+	descriptor_length = buff[17]; //8
+	service_type = buff[18]; //8
+	service_provider_name_length = buff[19]; //8
+	char temp0[service_provider_name_length + 1];
+	temp0[service_provider_name_length] = '\0';
+	memcpy(temp0, &buff[20], service_provider_name_length);
+	provider_name = temp0;
+
+	service_name_length = buff[20 + service_provider_name_length]; //8
+	char temp1[service_name_length + 1];
+	temp1[service_name_length] = '\0';
+	memcpy(temp1, &buff[21 + service_provider_name_length],
+			service_name_length);
+	service_name = temp1;
+
+	int offset = 21 + service_provider_name_length + service_name_length;
+	CRC_32 = (buff[offset] << 24) + (buff[offset + 1] << 16)
+			+ (buff[offset + 2] << 8) + (buff[offset + 3]);
+}
+
+/**
+ * 解析PAT，获取节目标号和ID的对应关系
+ * @param buff
+ */
+void ts_pat::parse(const unsigned char* buff) {
+	buff++;
+	table_id = buff[0]; //8; //固定为0x00，标志是该表是PAT
+	section_syntax_indicator = (buff[1] & 0x80) >> 7; //1; //段语法标志位，固定为1
+	zero = (buff[1] & 0x40) >> 6; //1; //0
+	reserved_0 = (buff[1] & 0x30) >> 4; //2; //保留位
+	section_length = ((buff[1] & 0x0f) << 8) + buff[2]; //12;//表示有用的字节数，包括CRC32
+	transport_stream_id = (buff[3] << 8) + buff[4]; //16;//该传输流的ID，区别于一个网络中其它多路复用的流
+	reserved_1 = (buff[5] & 0xc0) >> 6; //2; //保留位
+	version_number = (buff[5] & 0x3e) >> 1; //5; //范围0-31，表示PAT的版本号
+	current_next_indicator = buff[5] & 0x01; // 1; //发送的PAT是当前有效还是下一个PAT有效
+	section_number = buff[6]; //8; //分段的号码。PAT可能分为多段传输，第一段为00，以后每个分段加1，最多可能有256个分段
+	last_section_number = buff[7]; //8; //最后一个分段的号码
+
+	int crc32 = section_length - 1;
+	CRC_32 = (buff[crc32] << 24) + (buff[crc32 + 1] << 16)
+			+ (buff[crc32 + 2] << 8) + buff[crc32 + 3];
+
+	int pgm_size = (section_length - 9) / 4;
+	for (int i = 0; i < pgm_size; i++) {
+		pgm_table t;
+		t.program_number = (buff[8 + 4 * i] << 8) + buff[9 + 4 * i]; //16;
+		t.reserved_3 = (buff[10 + 4 * i] & 0xe0) >> 5; //3;
+		t.program_map_PID = ((buff[10 + 4 * i] & 0x1f) << 8) + buff[11 + 4 * i]; //13
+		pgms[t.program_number] = t;
+	}
+}
+
+/**
+ * 通过PID查询是否存在对应的节目
+ * @param pid
+ * @return
+ */
+bool ts_pat::is_pmt(uint16 pid) {
+	for (auto & ele : pgms) {
+		if (ele.second.program_map_PID == pid)
+			return true;
+	}
+	return false;
+}
+
+/**
+ * 解析PMT，获取流的信息.
+ * @param buff
+ */
+void ts_pmt::parse(const unsigned char* buff) {
+	buff++;
+	table_id = buff[0]; //8
+	section_syntax_indicator = (buff[1] & 0x80) >> 7; //1
+	zero = (buff[1] & 0x40) >> 6; //1
+	reserved_0 = (buff[1] & 0x30) >> 4; //2
+	section_length = ((buff[1] & 0x0f) << 8) + buff[2]; //12
+	program_number = (buff[3] << 8) + buff[4]; //16
+	reserved_1 = (buff[5] & 0xc0) >> 6; //2
+	version_number = ((buff[5] & 0x3e) >> 1); //5
+	current_next_indicator = buff[5] & 0x01; //1
+	section_number = buff[6]; //8
+	last_section_number = buff[7]; //8
+	reserved_2 = (buff[8] & 0xe0) >> 5; //3
+	PCR_PID = ((buff[8] & 0x1f) << 8) + buff[9]; //13
+	reserved_3 = (buff[10] & 0xf0) >> 4; //4
+	program_info_length = ((buff[10] & 0x0f) << 8) + buff[11]; //12
+
+	int data_len = section_length + 3;
+	CRC_32 = (buff[data_len - 4] << 24) + (buff[data_len - 3] << 16)
+			+ (buff[data_len - 2] << 8) + buff[data_len - 1];
+	int info_start = 12 + program_info_length;
+	int info_end = data_len - 4;
+	for (int pos = info_start; pos < info_end;) {
+		if (pos + 5 > info_end)
+			break;
+		pmt_stream_info info;
+		info.stream_type = buff[pos];
+		info.reserved_0 = (buff[pos + 1] & 0xe0) >> 5;
+		info.elementary_PID = ((buff[pos + 1] & 0x1f) << 8) + buff[pos + 2];
+		info.reserved_1 = (buff[pos + 3] & 0xf0) >> 4;
+		info.ES_info_length = ((buff[pos + 3] & 0x0f) << 8) + buff[pos + 4];
+		pos = pos + info.ES_info_length + 5;
+		stream_info[info.elementary_PID] = info;
+	}
+}
+
+/**
+ * 通过PID查询是否存在对应的流
+ * @param pid
+ * @return
+ */
+bool ts_pmt::is_element_pid(uint16 pid) {
+	return (stream_info.find(pid) != stream_info.end());
+}
+
+/**
+ * Constructor
+ * @param id: stream id
+ * @param s_type: stream type 0x1b h264; 0xf aac; 0x27 h265
+ * @param cb: callback function
+ * @param file_path: dump stream to file in to the path if not eq nullptr
+ */
+stream::stream(uint16 id, uint8 s_type, Data_CB cb, const char* file_path) {
+	_id = id;
+	_stream_type = stream_type(s_type);
+	_cb = cb;
+	if (file_path) {
+		char file[100] = { 0 };
+		const char* file_type;
+		switch (_stream_type) {
+		case h264:
+			file_type = "h264";
+			break;
+		case aac:
+			file_type = "aac";
+			break;
+		case h265:
+			file_type = "h265";
+			break;
+		default:
+			file_type = "unknown";
+			break;
+		}
+		sprintf(file, "%s%d.%s", file_path, (int) _id, file_type);
+		_file = fopen(file, "w+");
+	}
+}
+
+/**
+ * Destructor
+ */
+stream::~stream() {
+	if (_file)
+		fclose(_file);
+	if (_frame_data)
+		delete[] _frame_data;
+}
+
+/**
+ * 解析一帧aac数据
+ * @param data: 输入数据buffer
+ * @param data_len: 输入数据长度
+ * @param start_pos： 第一帧开始位置
+ * @param buffer_len：第一帧数据长度
+ * @return 0成功
+ */
+int stream::get_aac_buffer(const unsigned char* data, int data_len,
+		int& start_pos, int& buffer_len) {
+	//各个sample对应的每帧acc的pts
+	static int addPts[13] = { 960, 1045, 1440, 1920, 2089, 2880, 3840, 4180,
+			5760, 7680, 8359, 11520, 12539 };
+	start_pos = buffer_len = 0;
+	int i = 0;
+	for (i = 0; i < (data_len - 9); i++) {
+		//aac包头判断
+		if ((data[i] & 0xff) == 0xff && (data[i + 1] & 0xf0) == 0xf0) {
+			int hi = data[i + 3] & 0x03;
+			int mid = data[i + 4] & 0xff;
+			int low = data[i + 5] & 0xe0;
+			int bufferlen = ((hi << 16) + (mid << 8) + (low)) >> 5;
+			int sample_rate = (data[i + 2] & 0x3c) >> 2;
+			//数据传输有丢失 导致
+			if(bufferlen<=0){
+				return -2;
+			}
+
+			//int frames = (data[i + 6] & 0xc0);
+			//int header_len = (data[i+1]&0x01)==1?7:9;
+			//如果检测到的数据长度恰好在边界上，则直接取数据
+			if (i + bufferlen + 1 == data_len || i + bufferlen == data_len) {
+				start_pos = i;
+				buffer_len = bufferlen;
+				if ((sample_rate >= 0 && sample_rate < 13)) {
+					_pts += _additonal_pts;
+					_additonal_pts = addPts[sample_rate];
+				}
+				return 1;
+			}
+			//如果检测到的数据长度比送进的数据长度长，则返回检测失败
+			if (i + bufferlen + 1 > data_len)
+				return -1;
+
+			//正常情况一帧的结束，后面紧接着一帧的开始
+			if ((data[i + bufferlen] & 0xff) == 0xff
+					&& (data[i + bufferlen + 1] & 0xf0) == 0xf0) {
+				start_pos = i;
+				buffer_len = bufferlen;
+				if ((sample_rate >= 0 && sample_rate < 13)) {
+					_pts += _additonal_pts;
+					_additonal_pts = addPts[sample_rate];
+				}
+				return 1;
+			}
+			return -1;
+		}
+	}
+	return -1;
+}
+
+/**
+ * 解析acc
+ * @param s_data
+ */
+void stream::parse_aac(stream_data* s_data) {
+	//判断是否有PES包，如果有就把上一次pes包之后的数据剩下的反给回调函数
+	//并清空数据
+	if (s_data->_flag == 0x01 && _frame_len != 0) {
+		stream_data s(s_data->_id, 0, s_data->_s_type, _frame_data, _frame_len,
+				_pre_pts, _pre_dts);
+		_cb(&s);
+		memset(_frame_data, 0, BUFFER_SIZE);
+		_frame_len = 0;
+	}
+
+	int start_pos = 0, len = 0, offset = 0;
+	memcpy(_frame_data + _frame_len, s_data->_data, s_data->_len);
+	_frame_len += s_data->_len;
+	while (true) {
+		auto ret = get_aac_buffer(_frame_data + offset, _frame_len - offset,
+				start_pos, len);
+		if (ret >= 0) {
+			stream_data s(s_data->_id, 0, s_data->_s_type,
+					_frame_data + offset + start_pos, len, _pts, _dts);
+			_cb(&s);
+			offset += len;
+		} else {
+			//将剩余数据放到buffer的最前面
+			if(ret==-2)
+				offset+=2;
+			memmove(_frame_data, _frame_data + offset, _frame_len - offset);
+			memset(_frame_data + offset, 0, offset);
+			_frame_len -= offset;
+			break;
+		}
+	}
+}
+
+/**
+ * 解析图像
+ * @param s_data
+ */
+void stream::parse_pictrue(stream_data* s_data) {
+	//两个pes包之间的间隔为图像的数据
+	if (s_data->_flag == 0x01 && _frame_len != 0) {
+		if (_cb) {
+			stream_data s(s_data->_id, 0, s_data->_s_type, _frame_data,
+					_frame_len, _pre_pts, _pre_dts);
+			_cb(&s);
+		}
+		_frame_len = 0;
+		memset(_frame_data, 0, BUFFER_SIZE);
+		//return;
+	}
+	memcpy(_frame_data + _frame_len, s_data->_data, s_data->_len);
+	_frame_len += s_data->_len;
+}
+
+/**
+ * 解析负载数据
+ * @param s_data
+ */
+void stream::parse_buffer(stream_data* s_data) {
+	switch (s_data->_s_type) {
+	case ts::invalid:
+		break;
+	case ts::aac:
+		parse_aac(s_data);
+		break;
+	case ts::h264:
+		parse_pictrue(s_data);
+		break;
+	case ts::h265:
+		parse_pictrue(s_data);
+		break;
+	}
+}
+
+/**
+ * 解析一个流数据
+ * @param buff: 数据
+ * @param buff_len: 数据长度
+ * @param pes 是否有pes包
+ */
+void stream::parse(const unsigned char* buff, int buff_len, bool pes) {
+	//pes标志位
+	uint32 packet_start_code_prefix =
+			(buff_len >= 3) ? (buff[0] << 16) + (buff[1] << 8) + buff[2] : 0;
+	uint8 startpos = 0;
+	if (pes && packet_start_code_prefix == 1) {
+		_stream_id = buff[3];
+		//uint16 PES_packet_length = (buff[4] << 8) + buff[5];
+		uint8 PTS_DTS_flags = (buff[7] & 0xc0) >> 6;
+		uint8 PES_header_data_length = buff[8];
+		//流数据的起始位置
+		startpos = 9 + PES_header_data_length;
+		uint64 pts = 0, dts = 0;
+		auto decode_pts = [](const unsigned char* p) {
+			uint64 pts = (((uint64)p[0] & 0x0e) << 29);
+			pts |= ((p[1] & 0xff) << 22);
+			pts |= ((p[2] & 0xfe) << 14);
+			pts |= ((p[3] & 0xff) << 7);
+			pts |= ((p[4] & 0xfe) >> 1);
+			return pts;
+		};
+		switch (PTS_DTS_flags) {
+		case 3: // pts;dts
+			pts = decode_pts(buff + 9);
+			dts = decode_pts(buff + 14);
+			break;
+		case 2: //pts only
+			pts = decode_pts(buff + 9);
+			//if (_stream_type != aac)
+				dts = pts;
+			break;
+		}
+		_pre_pts = _pts;
+		_pre_dts = _dts;
+		if (_last_pts < pts)
+			_last_pts = pts;
+		_pts = pts;
+		//if (_dts < dts)
+			_dts = dts;
+		_additonal_pts = 0;
+	}
+
+	//h264数据偏移
+	if (_stream_type == 27) {
+		if (buff[startpos] == 0 && buff[startpos + 1] == 0
+				&& buff[startpos + 2] == 1) {
+			startpos += 3;
+			uint16 type = buff[startpos] & 0x1f;
+			if (type == 9)
+				startpos += 2;
+		}
+
+		if (buff[startpos] == 0 && buff[startpos + 1] == 0
+				&& buff[startpos + 2] == 0 && buff[startpos + 3] == 1) {
+			startpos += 4;
+			uint16 type = buff[startpos] & 0x1f;
+			if (type == 9)
+				startpos += 2;
+		}
+	}
+
+	int data_len = buff_len - startpos;
+	stream_data s_data(_id,
+			(pes && packet_start_code_prefix == 1) == true ? 0x01 : 0x10,
+			_stream_type, &buff[startpos], data_len, _pts, _dts);
+	parse_buffer(&s_data);
+
+	if (_file)
+		fwrite(&buff[startpos], 1, data_len, _file);
+}
+
+/**
+ * Destructor
+ * @param cb:回调函数
+ * @param file_path：需要dump流文件添加路径,如果不需要=nullptr
+ */
+demuxer::demuxer(Data_CB cb, const char* file_path) :
+		m_cb(cb), _file_path(file_path) {
+}
+
+/**
+ * 判断流是否存在
+ * @param pid
+ * @return
+ */
+bool demuxer::has_stream(int pid) {
+	return (m_streams.find(pid) != m_streams.end());
+}
+
+/**
+ * put一帧ts包,188byte
+ * @param buff
+ * @return
+ */
+int demuxer::put_buffer(const unsigned char* buff) {
+	if (buff == nullptr)
+		return 0;
+
+	ts_header tshdr(buff);
+	if (tshdr.playload_offset < 0)
+		return tshdr.playload_offset;
+
+	//数据结束地址
+	const uint8 * data_end = buff + 188;
+	//跳到playload data
+	buff += tshdr.playload_offset;
+	if (buff >= data_end)
+		return -4;
+
+	//0表示pat
+	if (tshdr.pid == 0x0) {
+		m_psi._pat.parse(buff);
+		return 0;
+	}
+	//11表示pat
+	if (tshdr.pid == 0x11) {
+		m_psi._sdt.parse(buff);
+		return 0;
+	}
+	//PMT
+	if (m_psi._pat.is_pmt(tshdr.pid)) {
+		m_psi._pmt.parse(buff);
+		return 0;
+	}
+	//判断是否为流ID
+	if (m_psi._pmt.is_element_pid(tshdr.pid)) {
+		//如果流不存在则创建
+		if (!has_stream(tshdr.pid)) {
+			auto id = m_psi._pmt.stream_info[tshdr.pid].elementary_PID;
+			auto s_type = m_psi._pmt.stream_info[tshdr.pid].stream_type;
+			m_streams[tshdr.pid] = std::make_shared<stream>(id, s_type, m_cb,
+					_file_path);
+		}
+		m_streams[tshdr.pid]->parse(buff, data_end - buff,
+				tshdr.payload_unit_start_indicator);
+		return 0;
+	}
+	printf("ignore pid:%x", tshdr.pid);
+	return 0;
+}
+
+} /* namespace ts */
---
+++ src/ts.h
@@ -0,0 +1,241 @@
+/*
+ * ts.h
+ *
+ *  Created on: 2017年8月3日
+ *      Author: root
+ */
+
+#ifndef TS_H_
+#define TS_H_
+#include <stdio.h>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <functional>
+using namespace std;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+namespace ts {
+enum stream_type {
+	invalid = -1, h264 = 0x1b, aac = 0xf, h265 = 0x27
+};
+
+struct stream_data {
+	int _id = 0; //stream id
+	int _flag = 0; //flag(0x00:invalid;0x01:start，has pes package;0x10:transporting)
+	stream_type _s_type = invalid;
+	const uint8* _data = 0; //data
+	int _len = 0; //data length
+	long long _pts = 0; //pts
+	long long _dts = 0; //dts
+	stream_data(int id, int flag, stream_type s_type, const uint8* data,
+			int len, long long pts, long long dts) :
+			_id(id), _flag(flag), _s_type(s_type), _data(data), _len(len), _pts(
+					pts), _dts(dts) {
+	}
+};
+
+typedef std::function<void(stream_data*)> Data_CB;
+
+struct pgm_table {
+	uint16 program_number = 0; //16;
+	uint8 reserved_3 = 0; //3;
+	uint16 program_map_PID = 0; //13
+};
+
+struct pmt_stream_info {
+	uint8 stream_type; //8
+	uint8 reserved_0; //3
+	uint16 elementary_PID; //13
+	uint8 reserved_1; //4
+	uint16 ES_info_length; //12
+};
+
+class ts_header {
+public:
+	ts_header() = default;
+	ts_header(const unsigned char* buff) {
+		parse(buff);
+	}
+	~ts_header() = default;
+	void parse(const unsigned char* buff);
+public:
+	uint8 sync_byte = 0; //8bit,同步字节，固定为0x47，表示后面的是一个TS分组，当然， 后面包中的数据是不会出现0x47的
+	bool transport_error_indicator = false; //1bit,传输错误标志位，一般传输错误的话就不会处理这个包了
+	bool payload_unit_start_indicator = false; //1bit,有效负载的开始标志，根据后面有效负载的内容不同功能也不同
+	//payload_unit_start_indicator为1时，在前4个字节之后会有一个调整字节， 它的数值决定了负载内容的具体开始位置。
+	bool transport_priority = false;      //1bit,传输优先级位，1表示高优先级
+	uint16 pid = 0; //有效负载数据的类型 13bit
+	uint8 transport_scrambling_control = 0; //2bit,加密标志位,00表示未加密
+	uint8 adaption_field_control = 0; //2bit,调整字段控制,。01仅含有效负载，10仅含调整字段，11含有调整字段和有效负载。为00的话解码器不进行处理。
+	uint8 continuity_counter = 0; //4bit,一个4bit的计数器，范围0-15
+
+	//不再标准中，自己添加一个playload data offset方便计算,同时如果小于0则认为解析失败
+	short playload_offset = 0;
+
+};
+
+class ts_sdt {
+	//sercive description table
+public:
+	ts_sdt() = default;
+	ts_sdt(const unsigned char *buff) {
+		parse(buff);
+	}
+	~ts_sdt() = default;
+	void parse(const unsigned char* buff);
+public:
+	uint8 table_id = 0; //8
+	bool section_syntax_indicator = false; //1 通常设为“1”
+	bool reserved_future_use_0 = false; //1
+	uint8 reserved_0 = 0; //2
+	uint16 section_length = 0; //12
+	uint16 transport_stream_id = 0; //16 给出TS识别号
+	uint8 reserved_1 = 0; //2
+	uint8 version_number = 0; //5
+	bool current_next_indicator = false; //1
+	uint8 section_number = false; //8
+	uint8 last_section_number = 0; //8
+	uint16 original_nerwork_id = 0; //16
+	uint8 reserved_future_use_1 = 0; //8
+
+	uint16 service_id = 0; //16
+	uint8 reserved_future_use_2 = 0; //6
+	bool EIT_schedule_flag = false; //1
+	bool EIT_present_following_flag = false; //1
+	uint8 running_status = 0; //3
+	bool freed_CA_mode = false; //1
+	uint16 descriptors_loop_length = 0; //12
+
+	uint8 descriptor_tag = 0; //8
+	uint8 descriptor_length = 0; //8
+	uint8 service_type = 0; //8
+	uint8 service_provider_name_length = 0; //8
+	string provider_name;
+	uint8 service_name_length = 0; //8
+	string service_name;
+
+	uint32 CRC_32 = 0; //32
+};
+
+class ts_pat {
+	//iso 2.4.4.3
+public:
+	ts_pat() = default;
+	~ts_pat() = default;
+	ts_pat(const unsigned char* buff) {
+		parse(buff);
+	}
+	void parse(const unsigned char* buff);
+	bool is_pmt(uint16 pid);
+public:
+	uint8 table_id = 0; //8; //固定为0x00，标志是该表是PAT
+	bool section_syntax_indicator = false; //1; //段语法标志位，固定为1
+	bool zero = false; //1; //0
+	uint8 reserved_0 = 0; //2; //保留位
+	uint16 section_length = 0; //12;//表示有用的字节数，包括CRC32
+	uint16 transport_stream_id = 0; //16;//该传输流的ID，区别于一个网络中其它多路复用的流
+	uint8 reserved_1 = 0; //2; //保留位
+	uint8 version_number = 0; //5; //范围0-31，表示PAT的版本号
+	bool current_next_indicator = false; // 1; //发送的PAT是当前有效还是下一个PAT有效
+	uint8 section_number = 0; //8; //分段的号码。PAT可能分为多段传输，第一段为00，以后每个分段加1，最多可能有256个分段
+	uint8 last_section_number = 0; //8; //最后一个分段的号码
+
+	map<int, pgm_table> pgms;
+
+	uint32 CRC_32 = 0; //32;
+};
+
+class ts_pmt {
+public:
+	ts_pmt() = default;
+	~ts_pmt() = default;
+	void parse(const unsigned char* buff);
+	bool is_element_pid(uint16 pid);
+public:
+	uint8 table_id = 0; //8
+	bool section_syntax_indicator = false; //1
+	bool zero = false;
+	uint8 reserved_0 = 0; //2
+	uint16 section_length = 0; //12
+	uint16 program_number = 0; //16
+	uint8 reserved_1 = 0; //2
+	uint8 version_number = 0; //5
+	bool current_next_indicator = false; //1
+	uint8 section_number = 0; //8
+	uint8 last_section_number = 0; //8
+	uint8 reserved_2 = 0; //3
+	uint16 PCR_PID = 0; //13
+	uint8 reserved_3 = 0; //4
+	uint16 program_info_length = 0; //12
+	std::map<uint16, pmt_stream_info> stream_info;
+	uint32 CRC_32 = 0; //32
+};
+
+class ts_psi {
+public:
+	ts_psi() = default;
+	~ts_psi() = default;
+	ts_sdt _sdt;
+	ts_pat _pat;
+	ts_pmt _pmt;
+};
+
+class stream {
+#define BUFFER_SIZE 1024*1024*4
+public:
+	stream() = default;
+	stream(uint16 id, uint8 s_type, Data_CB cb, const char* file_path);
+	~stream();
+	void parse(const unsigned char* buff, int buff_len, bool pes);
+private:
+	int get_aac_buffer(const unsigned char* data, int data_len, int& start_pos,
+			int& buffer_len);
+	void parse_buffer(stream_data* s_data);
+	void parse_pictrue(stream_data* s_data);
+	void parse_aac(stream_data* s_data);
+	uint16 _id = 0;
+	stream_type _stream_type = invalid; //0x1b h264; 0xf aac; 0x27 h265
+	uint8 _stream_id = 0; //audio:110x xxxx; video:1110 xxxx
+
+	uint64 _pts = 0;
+	uint64 _pre_pts = 0;
+	uint64 _last_pts = 0;
+	uint64 _dts = 0;
+	uint64 _pre_dts = 0;
+
+	Data_CB _cb = nullptr;
+	FILE* _file = nullptr;
+
+	uint8* _frame_data = new uint8[BUFFER_SIZE];
+	uint32 _frame_len = 0;
+	uint32 _additonal_pts = 0; //在音频流中两个pes包之间有多个音频帧，从接收到pes包之后的第二帧pts需要额外增加
+};
+
+class demuxer {
+public:
+	demuxer() = default;
+	demuxer(Data_CB cb, const char* file_path = nullptr);
+	~demuxer() = default;
+	int put_buffer(const unsigned char* buff);
+private:
+	bool has_stream(int pid);
+	map<int, std::shared_ptr<stream>> m_streams;
+	ts_psi m_psi;
+	Data_CB m_cb = nullptr;
+	const char* _file_path = nullptr;
+};
+
+} /* namespace ts */
+
+#endif /* TS_H_ */
